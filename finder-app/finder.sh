6e0988c8da6032f3642392d7f5796712b018d121
Goyaqua <bengisu644@gmail.com>
2025-04-07 04:29:19 +0300

    Fix: Updated finder.sh, manual-linux.sh, and Makefile for proper rootfs dependencies

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ $# -ne 2 ]; then
 	echo "Error: Incorrect number of arguments. Usage: $0 <filesdir> <searchstr>"
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -38,81 +38,78 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
   git checkout ${KERNEL_VERSION}
 
   make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
-
-  sed -i 's/YYLTYPE yylloc;/extern YYLTYPE yylloc;/' scripts/dtc/dtc-lexer.l
-
   make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-  make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} Image
-
-  make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+  make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+  make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
-cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+	  echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-rm -rf ${OUTDIR}/rootfs
-mkdir ${OUTDIR}/rootfs
-cd ${OUTDIR}/rootfs
-mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
-mkdir -p usr/bin usr/lib usr/sbin var/log
-
+mkdir -pv ${OUTDIR}/rootfs/{bin,dev,etc,home,lib,lib64,proc,sbin,sys,tmp}
+mkdir -pv ${OUTDIR}/rootfs/usr/{bin,lib,sbin}
+mkdir -pv ${OUTDIR}/rootfs/var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]; then
     git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    make distclean
-    make defconfig
+  make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} distclean
+  make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
 else
     cd busybox
 fi
 
-
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} -j$(nproc)
 make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
-echo "Adding library dependencies to rootfs"
+SYSROOT=$(aarch64-none-linux-gnu-gcc -print-sysroot)
+
+# TODO: Add library dependencies to rootfs
 cd ${OUTDIR}/rootfs
+cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+
+echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
-cp -L ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
-cp -L ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
-cp -L ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
-cp -L ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+echo "Creating device nodes..."
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
 
-if [ ! -e dev/null ]; then
-  sudo mknod -m 666 dev/null c 1 3
-fi
-if [ ! -e dev/console ]; then
-  sudo mknod -m 622 dev/console c 5 1
-fi
+echo "Copying user applications and scripts..."
+mkdir -p ${OUTDIR}/rootfs/home
 
 cd ${FINDER_APP_DIR}
 make clean
-make CROSS_COMPILE=${CROSS_COMPILE}
+make CROSS_COMPILE=$CROSS_COMPILE
 
-cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home
-cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home
-cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home
-cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home
-cp -r ${FINDER_APP_DIR}/conf/ ${OUTDIR}/rootfs/home
+cp -ar ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home/
+cp -ar ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home/
+cp -ar ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/
+cp -ar ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home/
+cp -ar ${FINDER_APP_DIR}/../conf ${OUTDIR}/rootfs/home/
 
+# Fix path inside finder-test.sh
 sed -i 's|../conf/assignment.txt|conf/assignment.txt|' ${OUTDIR}/rootfs/home/finder-test.sh
 
+echo "Setting ownership and creating init link..."
 cd ${OUTDIR}/rootfs
 sudo chown -R root:root .
 sudo ln -sf bin/sh init
 
 echo "Creating initramfs.cpio.gz..."
-find . | cpio -H newc -ov --owner root:root | gzip > ${OUTDIR}/initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root 2>/dev/null | gzip > ${OUTDIR}/initramfs.cpio.gz
+echo "Done: ${OUTDIR}/initramfs.cpio.gz created"
 
